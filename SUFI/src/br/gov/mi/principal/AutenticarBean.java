c8bb95ff0fb4e557f48e30cd74730ac75eed6c85
pablomaiden <pablomaiden@gmail.com>
2013-04-29 17:48:46 -0300

    Classe de autenticação

---
+++ SUFI/src/br/gov/mi/principal/AutenticarBean.java
@@ -0,0 +1,21 @@
+package br.gov.mi.principal;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class AutenticarBean extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.principal);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {		
+		getMenuInflater().inflate(R.menu.principal, menu);
+		return true;
+	}
+
+}
